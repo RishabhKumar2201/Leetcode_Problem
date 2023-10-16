41605607ca13b5324435df932f749d619ccba068
Rishabh Kumar <116102585+RishabhKumar2201@users.noreply.github.com>
2023-10-16 11:26:32 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.java
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.java
@@ -54,10 +54,10 @@ class DriverClass
 
 //User function Template for Java
 
-/*class Pair{
+class Pair{
     int node;
     int distance;
-    Pair(int node, int distance){
+    Pair(int distance, int node){
         this.node = node;
         this.distance = distance;
     }
@@ -96,43 +96,3 @@ class Solution
     }
 }
 
-*/
-class Pair {
-    int node;
-    int distance;
-
-    Pair(int distance, int node) {
-        this.node = node;
-        this.distance = distance;
-    }
-}
-
-class Solution {
-    // Function to find the shortest distance of all the vertices
-    // from the source vertex S.
-    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
-        int[] dist = new int[V];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[S] = 0;
-        pq.add(new Pair(0, S));
-
-        while (!pq.isEmpty()) {
-            int dis = pq.peek().distance;
-            int node = pq.peek().node;
-            pq.poll();
-
-            for (int i = 0; i < adj.get(node).size(); i++) {
-                int edgeWeight = adj.get(node).get(i).get(1);
-                int adjNode = adj.get(node).get(i).get(0);
-
-                if (dis + edgeWeight < dist[adjNode]) {
-                    dist[adjNode] = dis + edgeWeight;
-                    pq.add(new Pair(dis + edgeWeight, adjNode));
-                }
-            }
-        }
-
-        return dist;
-    }
-}
